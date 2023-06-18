923b522e3a04478172e74168aad95c51776b25f6
KarlTred265 Tred <tred26559@gmail.com>
2023-06-18 19:36:34 +0000

    add navbar menu

--- tennisblog/urls.py
+++ tennisblog/urls.py
@@ -4,5 +4,7 @@ from tennisblog import views
 
 urlpatterns = [
     path('',views.index,name="index"),
-    
+    path('about/',views.about,name="about"),
+    path('contact/',views.contact,name="contact"),
+
 ]
